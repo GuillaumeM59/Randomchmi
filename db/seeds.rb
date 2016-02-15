0f0f84a80c88850b84855c45e428035c4c351093
GuillaumeM59 <manier-guillaume@orange.fr>
2016-02-15 16:04:19 +0100

    create Person and seed models, controllers and seed to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+a=["Guillaume",
+"Sophie",
+"Jade",
+"Philippe",
+"Anas",
+"Najib",
+"Marc",
+"Antoine",
+"Thierry",
+"Anthony",
+"Thomas",
+"Vincent",
+"Alexis",
+"Julie",
+"Sabrina",
+"Bertrand",
+"Naïma",
+"François",
+"Othman",
+"Caroline",
+"Didier",
+"Thanh",
+"Manoël"]
+
+23.times do |i|
+    us = Person.create(
+          name: "#{a[i]}",
+          email: "#{a[i]}@gmail.com",
+          tel: 612345678 ,
+          sensei: false ,
+          )
+    end
