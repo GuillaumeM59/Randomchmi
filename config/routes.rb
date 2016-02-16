6916515cdd92e6831ce3d7e85319f4a66db226fa
GuillaumeM59 <manier-guillaume@orange.fr>
2016-02-16 10:15:57 +0100

    updtae redirection after create new group, and update it

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :groups
   resources :people
 
-  root 'people#index'
+  root 'groups#index'
 
-  get 'groups' => 'groups#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
